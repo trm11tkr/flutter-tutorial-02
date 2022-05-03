f42a668101af514bce3cf2485324966a1bc07f8a
森田陸斗 <mrmrt1121@gmail.com>
2022-05-03 15:03:01 +0900

    first commit

---
+++ lib/call_sclean.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CallSclean extends StatelessWidget {
+  const CallSclean({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
